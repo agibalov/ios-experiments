9f3b7dc397d384fbf12c254b784f073836015617
Andrey Agibalov <andrey.agibalov@gmail.com>
2024-10-18 17:08:21 -0400

    Save progress

--- UiTestingExperiment/README.md
+++ UiTestingExperiment/README.md
@@ -2,6 +2,8 @@
 
 Figuring out Swift/SwiftUI testing.
 
+* `./tool.sh clean` - "clean" (what exactly does it do?)
+* `./tool.sh archive` - creates an app archive at `build/app.xarchive`
 * `./tool.sh test` - run all tests.
 * `./tool.sh test-unit` - run unit tests.
 * `./tool.sh test-ui` - run UI tests.
--- UiTestingExperiment/tool.sh
+++ UiTestingExperiment/tool.sh
@@ -4,7 +4,26 @@ PROJECT_NAME=UiTestingExperiment.xcodeproj
 SCHEME_NAME=UiTestingExperiment
 DESTINATION="platform=iOS Simulator,name=iPhone SE (3rd generation),OS=18.0"
 
-if [ "${command}" == "test" ]; then
+if [ "${command}" == "" ]; then
+  echo "No command specified"
+
+elif [ "${command}" == "clean" ]; then
+  xcodebuild \
+    -project ${PROJECT_NAME} \
+    -scheme ${SCHEME_NAME} \
+    -destination "${DESTINATION}" \
+    clean
+
+elif [ "${command}" == "archive" ]; then
+  xcodebuild \
+    -project ${PROJECT_NAME} \
+    -scheme ${SCHEME_NAME} \
+    -configuration Release \
+    -destination "generic/platform=iOS" \
+    -archivePath ./build/app.xcarchive \
+    archive
+
+elif [ "${command}" == "test" ]; then
   xcodebuild \
     -project ${PROJECT_NAME} \
     -scheme ${SCHEME_NAME} \
